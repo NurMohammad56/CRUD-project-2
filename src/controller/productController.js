903e4e2ced7429f65c916f12b5b7151358ed999b
Nur Mohammad <milonsardar56@gmail.com>
2023-12-04 15:22:19 +0600

    Api configure complete for CRUD

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -17,7 +17,7 @@ exports.ReadProduct = async (req, res)=>{
         let query = {};
         let projection = "productName productCode img unitPrice qty totalPrice";
         let result = await ProductModel.find(query, projection);
-        res.status(200).json({status:"Product Created", data:result})
+        res.status(200).json({status:"Success", data:result})
     }catch(e){
         res.status(200).json({status:"Unsuccess", data:e})
 }
@@ -30,7 +30,19 @@ exports.UpdateProduct=async (req, res)=>{
         let query = {_id:id}
         let reqBody = req.body;
         let result = await ProductModel.updateOne(query, reqBody);
-        res.status(200).json({status:"Product Created", data:result})
+        res.status(200).json({status:"Product Update Success", data:result})
+    } catch (e) {
+        res.status(200).json({status:"Unsuccess", data:e})
+    }
+}
+
+// D=DELETE
+exports.DeleteProduct=async (req, res)=>{
+    try {
+        let id = req.params.id;
+        let query = {_id:id}
+        let result = await ProductModel.updateOne(query);
+        res.status(200).json({status:"Product Delete Success", data:result})
     } catch (e) {
         res.status(200).json({status:"Unsuccess", data:e})
     }
--- src/router/api.js
+++ src/router/api.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const productController = require("../controller/productController");
 const router = express.Router();
 
 // Api Routing end point
+// CREATE
+router.post("/CreateProduct", productController.CreateProduct);
 
+// READ
+router.get("/ReadProduct", productController.ReadProduct);
+
+// Update
+router.get("/UpdateProduct", productController.UpdateProduct);
+
+// DELETE
+router.get("/DeleteProduct", productController.DeleteProduct);
 
 module.exports=router;
